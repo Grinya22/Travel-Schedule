2fbf80614d44f8db023465f6bd7507737d44006c
Greg <gregory.vanyurin@icloud.com>
2025-10-22 18:46:53 +0400

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by gregory maier on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
